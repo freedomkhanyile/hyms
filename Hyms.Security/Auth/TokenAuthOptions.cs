70637496e47fe1f498640f50f8eb711a9aa1ffa0
Freedom Khanyile <freedom.khanyile1@gmail.com>
2021-06-17 21:26:09 +0200

    Security context with its helpers done.

---
+++ Hyms.Security/Auth/TokenAuthOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hyms.Security.Auth
+{
+    public  class TokenAuthOptions
+    {
+        public static string Audience { get; } = "ExpensesAudience";
+        public static string Issuer { get; } = "ExpensesIssuer";
+        public static RsaSecurityKey Key { get; } = new RsaSecurityKey(RSAKeyHelper.GenerateKey());
+        public static SigningCredentials SigningCredentials { get; } = new SigningCredentials(Key, SecurityAlgorithms.RsaSha256Signature);
+
+        public static TimeSpan ExpiresSpan { get; } = TimeSpan.FromMinutes(30);
+        public static string TokenType { get; } = "Bearer";
+    }
+}
--- Hyms.Security/Auth/TokenBuilder.cs
+++ Hyms.Security/Auth/TokenBuilder.cs
@@ -1,14 +1,39 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
 
 namespace Hyms.Security.Auth
 {
     public class TokenBuilder : ITokenBuilder
     {
+ 
         public string Build(string name, string[] roles, DateTime expireDate)
         {
-            throw new NotImplementedException();
+            var handler = new JwtSecurityTokenHandler();
+
+            var claims = new List<Claim>();
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            ClaimsIdentity identity = new ClaimsIdentity(
+             new GenericIdentity(name, "Bearer"),
+             claims);
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = TokenAuthOptions.Issuer,
+                Audience = TokenAuthOptions.Audience,
+                SigningCredentials = TokenAuthOptions.SigningCredentials,
+                Subject = identity,
+                Expires = expireDate
+            });
+
+            return handler.WriteToken(securityToken);
         }
     }
 }
